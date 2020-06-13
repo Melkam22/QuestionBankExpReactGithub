dba0e6121acb1e7d5dffeaa5efaeae385cfc6735
Ashu <ashenshi@gmail.com>
2020-06-13 12:12:54 +0200

    post route added on routes/allRouter.js

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -3,9 +3,23 @@ const router = express.Router();
 
 const quizeSchema = require('../models/schema'); 
  
-
+//test route
 router.get('/question', (req, res)=>{
     res.json('welcome to quiz challenge!')
+})
+
+//route to save data to database
+router.post('/question/save', (req, res)=>{
+    const data = req.body;
+    const newquizeSchema = new quizeSchema(data);//creating new instance of my model
+    newquizeSchema.save((error)=>{
+        if(error){
+            res.status(500).json({msg: 'Error on server occured.'})
+        } else {
+            res.json({msg: 'data from frontend is received'}); 
+        }
+    })
+     
 })
 
 
